f33993074973f638c280aa2300997add63ed2ded
SantoshVel <santoshvel555@gmail.com>
2024-12-10 11:07:33 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+setup(
+    name='Medicalbot',
+    version='0.0.0',
+    author='Santosh Vel',
+    author_email='santoshvel555@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
